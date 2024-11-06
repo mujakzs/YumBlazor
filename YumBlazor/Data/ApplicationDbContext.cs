c61b96e3ebbd02fc2aba5cd4c6445df762a5e599
mujakzs <andriangultiano14@gmail.com>
2024-11-06 22:44:05 +0800

    Create Category Model and Table

--- YumBlazor/Data/ApplicationDbContext.cs
+++ YumBlazor/Data/ApplicationDbContext.cs
@@ -5,5 +5,19 @@ namespace YumBlazor.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Category>().HasData(
+
+                new Category { CatId=1, Name="Appetizer"},
+                new Category { CatId=1, Name="Entree"},
+                new Category { CatId=1, Name="Dessert"}
+                );
+        }
     }
 }
---
+++ YumBlazor/Data/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace YumBlazor.Data;
+using System.ComponentModel.DataAnnotations;
+
+
+public class Category
+{
+    
+    public int CatId { get; set; }
+
+    [Required(ErrorMessage = "Please enter name...")]
+    public string Name { get; set; }
+}
