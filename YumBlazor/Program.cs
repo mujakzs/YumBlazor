2fb3ff22d4e5550cce5c998b62a0d1d3fcacd5a1
mujakzs <andriangultiano14@gmail.com>
2024-11-07 22:57:01 +0800

    Upsert Category Component

--- YumBlazor/Program.cs
+++ YumBlazor/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddControllersWithViews();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IdentityUserAccessor>();
