016b152238356ca78068bc0103556f9ece08775f
mujakzs <andriangultiano14@gmail.com>
2024-11-10 14:05:03 +0800

    Adding Interface

---
+++ YumBlazor/Repository/IRepository/IUserInfoRepository.cs
@@ -0,0 +1,21 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+    public interface IUserInfoRepository
+    {
+        public Task<UserInfo> CreateAsync(UserInfo user);
+
+        public Task<UserInfo> UpdateAsync(UserInfo user);
+
+        public Task<bool> DeleteAsync(int id);
+
+        public Task<UserInfo> GetAsync(int id);
+
+        public Task<IEnumerable<UserInfo>> GetAllAsync();
+
+
+
+
+    }
+}
---
+++ YumBlazor/Repository/UserInfoRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
+
+namespace YumBlazor.Repository
+{
+    public class UserInfoRepository : IUserInfoRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserInfoRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<UserInfo> CreateAsync(UserInfo user)
+        {
+            await _db.UserInfo.AddAsync(user);
+            await _db.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var user = await _db.UserInfo.FirstOrDefaultAsync(x => x.Id == id);
+            if (user != null)
+            {
+                _db.UserInfo.Remove(user);
+                return (await _db.SaveChangesAsync()) > 0;
+            }
+            return false;
+        }
+
+        public async Task<UserInfo> GetAsync(int id)
+        {
+            var user = _db.UserInfo.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return new UserInfo();
+            }
+            return user;
+
+        }
+
+        public async Task<IEnumerable<UserInfo>> GetAllAsync()
+        {
+            return await _db.UserInfo.ToListAsync();
+        }
+
+        public async Task<UserInfo> UpdateAsync(UserInfo user)
+        {
+            var userFromDb = await _db.UserInfo.FirstOrDefaultAsync(x => x.Id == user.Id);
+            if (userFromDb is not null)
+            {
+                userFromDb.FirstName = user.FirstName;
+                _db.UserInfo.Update(userFromDb);
+                await _db.SaveChangesAsync();
+                return userFromDb;
+            }
+
+            return user;
+        }
+    }
+}
